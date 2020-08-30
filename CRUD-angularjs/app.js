eed4a412250d534cb3ebb2446d612aec667830ad
bakti-widianto <bakti.widianto@gmail.com>
2020-08-30 13:55:39 +0700

    config database

--- CRUD-angularjs/app.js
+++ CRUD-angularjs/app.js
@@ -2,8 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Pool } = require('pg')
 
-var indexRouter = require('./routes/index');
+const pool = new Pool({
+  user: 'bakti',
+  host: 'localhost',
+  database: 'breaddb',
+  password: '112233',
+  port: 5432,
+})
+
+var breadRouter = require('./routes/bread');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -14,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/bread', breadRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
