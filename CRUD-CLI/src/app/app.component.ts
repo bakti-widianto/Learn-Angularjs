93a959d9da17f9ffe9dcf4b3917f1684b796b738
bakti-widianto <bakti.widianto@gmail.com>
2020-09-04 00:19:06 +0700

    read data done

--- CRUD-CLI/src/app/app.component.ts
+++ CRUD-CLI/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user'
+import { UserService } from './services/user.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CRUD-CLI';
+export class AppComponent implements OnInit{
+
+  users: User[]
+
+  constructor(private userService: UserService){ }
+
+  ngOnInit(){
+    this.users = this.getUsers();
+    console.log(this.users)
+  }
+
+  getUsers(): User[]{
+    return this.userService.getUsersFromData();
+
+  }
+
+  showAddForm(){
+    console.log('form add')
+  }
+
 }
